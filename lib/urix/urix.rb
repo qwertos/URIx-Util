99cacb78f0672e61897d4f094feb586a60e0d58c
Aaron Herting <aaron@herting.cc>
2014-02-20 11:31:09 -0500

    set output has some logic now

--- lib/urix/urix.rb
+++ lib/urix/urix.rb
@@ -6,12 +6,33 @@ module URIx
 
 	class URIx
 
-		attr_accessor :device
+		attr_accessor :usb, :device, :handle
+		attr_reader :pin_states
 		
 		def initialize
-			usb = LIBUSB::Context.new
-			@device = usb.devices(:idVendor => VENDOR_ID, :idProduct => PRODUCT_ID).first
+			@usb = LIBUSB::Context.new
+			@pin_states = 0x0
 		end
+
+		def init_interface
+			@device = usb.devices(:idVendor => VENDOR_ID, :idProduct => PRODUCT_ID).first
+			@handle = @device.open
+			
+			@handle.detach_kernel_driver(HID_INTERFACE)
+		end
+
+		def set_output pin, state
+			if ( @pin_states >> ( pin - 1 )).odd? && ( state == false ) or
+			   ( @pin_states >> ( pin - 1 )).even? && ( state == true ) then
+
+				mask = 0 + ( 1 << ( pin - 1 ))
+				@pin_states ^= mask
+			end
+		end
+
+			
+
+
 
 	end
 
